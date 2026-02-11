c9b8ed15a830b0a1dc0b4234d66f61fd5648d3da
durgapeasadkurma2015 <durgaprasadkurma2015@gmail.com>
2026-02-11 22:50:24 +0530

    H2-curd

---
+++ SpringBoot_H2-CURD/src/main/java/com/example/H2_curd/H2CurdApplication.java
@@ -0,0 +1,13 @@
+package com.example.H2_curd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2CurdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2CurdApplication.class, args);
+	}
+
+}
